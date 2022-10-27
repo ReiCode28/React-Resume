6c25f54b3c42d28aae5463189b3ec5370e6a660f
randy <randyr_1993@yahoo.com>
2022-10-27 15:53:42 -0400

    Fixed footer error

--- client/src/PortfolioContainer/Home/Footer/Footer.js
+++ client/src/PortfolioContainer/Home/Footer/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
     <div className="footer-container">
       <div className="footer-parent">
         <img
-          src={require("client/src/assets/Home/shape-bg.png").default}
+          src={require("../../../assets/fonts/shape-bg.png")}
           alt="no internet connection"
         />
       </div>
