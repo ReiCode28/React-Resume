1ea80265e776d820ef68f6c7f10a4fe20519e98a
randy <randyr_1993@yahoo.com>
2022-10-27 01:16:27 -0400

    Created react-typical animation + hire me and resume buttons

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Typical from 'react-typical'
 
 export default function Profile() {
   return (
@@ -6,11 +7,52 @@ export default function Profile() {
         <div className='profile-parent'>
             <div className='profile-details'>
                 <div className='colz'>
-                    <a href='#'>
+                    <a href='https://www.linkedin.com/in/randy-reina'>
                         <i className='fa fa-linkedin-square'></i>
                     </a>
-                    <a href='#'>
+                    <a href='https://github.com/ReiCode28'>
                         <i className='fa fa-github-square'></i>
+                    </a>
+                </div>
+                <div className="profile-details-name">
+                    <span className="primary-text">
+                        {" "}
+                        Hello, I'm <span className="highlighted-text">Randy</span>
+                    </span>
+                </div>
+                <div className='profile-details-role'>
+                    <span className='primary-text'>
+                        {" "}
+                        <h1>
+                            {" "}
+                            <Typical 
+                            loop={Infinity}
+                            steps={[
+                                "Creative 👨‍🎨",
+                                1500,
+                                "Visionary 😎",
+                                1500,
+                                "Passionate ❤️‍🔥",
+                                1500,
+                                "Life-long learner 📚",
+                                1500,
+                                "Critical Thinker 🧠",
+                                1500,
+                            ]}
+                            />
+                        </h1>
+                        <span className='profile-role-tagline'> 
+                            Building applications with front end and back end operations.
+                        </span> 
+                    </span>
+                </div>
+                <div className='profile-options'>
+                    <button className='btn primary-btn'>
+                        {""}
+                        Hire Me{" "}
+                    </button>
+                    <a href='RandyR_ResumeUpdated.pdf' download='RandyR_ResumeUpdated.pdf'>
+                    <button className='btn highlighted-btn'>Get Resume</button>
                     </a>
                 </div>
             </div>
