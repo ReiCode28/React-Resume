98327637ac699f431d4801ca128b64a65da5877f
randy <randyr_1993@yahoo.com>
2022-10-26 23:28:51 -0400

    Created PortfolioContainer file structure

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className='prifle-container'>
+        <div className='profile-parent'>
+            <div className='profile-details'>
+                <div className='colz'>
+                    <a href='#'>
+                        <i className='fa fa-linkedin-square'></i>
+                    </a>
+                    <a href='#'>
+                        <i className='fa fa-github-square'></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
