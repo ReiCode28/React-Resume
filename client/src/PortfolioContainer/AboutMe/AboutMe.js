27d255f8a48234de68c8fd3f3318e89dd343f02b
randy <randyr_1993@yahoo.com>
2022-10-31 03:22:15 -0400

    initializing the backend

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -1,7 +1,7 @@
-import React, {useEffect} from 'react'
-import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading'
-import ScrollService from '../../utilities/ScrollService'
-import Animations from '../../utilities/Animations'
+import React, { useEffect } from "react";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
 import "./AboutMe.css";
 
 export default function AboutMe(props) {
@@ -13,62 +13,71 @@ export default function AboutMe(props) {
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
   const SCREEN_CONSTANTS = {
-    description: "Software Engineer with experience in building full stack web applications in a collaborative environment; utilizing Agile methodologies and proper version control to complete projects in a timely manner. Background knowledge of backend - Java, Spring Boot and front end - Javascript, React, Express and NodeJS. Coachable and willing to be a valuable asset to an organization.",
-    highlights:{
-      bullets:[
+    description:
+      "Software Engineer with experience in building full stack web applications in a collaborative environment; utilizing Agile methodologies and proper version control to complete projects in a timely manner. Background knowledge of backend - Java, Spring Boot and front end - Javascript, React, Express and NodeJS. Coachable and willing to be a valuable asset to an organization.",
+    highlights: {
+      bullets: [
         "Constantly striving for improvement and learning",
         "Ability to work in large, collaborative teams to achieve organizational goals",
         "Strong creativity, problem-solving and critical thinking skills",
         "Ability to manage projects, mentor and help others",
       ],
-      heading: "Here are a few Highlights: "
-    }
-  }
-  const renderHighlight = ()=>{
-    return(
-      SCREEN_CONSTANTS.highlights.bullets.map((value, i)=>(
-        <div className='highlight' key={i}>
-          <div className='highlight-blob'></div>
-          <span>{value}</span>
-        </div>
-      ))
-    )
-  }
+      heading: "Here are a few Highlights: ",
+    },
+  };
+  const renderHighlight = () => {
+    return SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
 
   useEffect(() => {
     return () => {
-        /* UNSUBSCRIBE THE SUBSCRIPTIONS */
-        fadeInSubscription.unsubscribe();
-    }
-}, [fadeInSubscription]);
+      /* UNSUBSCRIBE THE SUBSCRIPTIONS */
+      fadeInSubscription.unsubscribe();
+    };
+  }, [fadeInSubscription]);
 
   return (
-    <div className='about-me-container screen-container fade-in' id={props.id || ""}>
-      <div className='about-me-parent'>
-        <ScreenHeading title={'About Me'} subHeading={'Why Choose Me?'}/>
-        <div className='about-me-card'>
-          <div className='about-me-profile'></div>
-          <div className='about-me-details'>
-            <span className='about-me-description'>{SCREEN_CONSTANTS.description}</span>
-            <div className='about-me-highlights'>
-              <div className='highlight-heading'>
+    <div
+      className="about-me-container screen-container fade-in"
+      id={props.id || ""}
+    >
+      <div className="about-me-parent">
+        <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"} />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">
+              {SCREEN_CONSTANTS.description}
+            </span>
+            <div className="about-me-highlights">
+              <div className="highlight-heading">
                 <span>{SCREEN_CONSTANTS.highlights.heading}</span>
               </div>
               {renderHighlight()}
             </div>
-            <div className='about-me-options'>
-            <button className='btn primary-btn'
-            onClick={() => ScrollService.scrollHandler.scrollToHireMe()}>
-                        {""}
-                        Hire Me{" "}
-                    </button>
-                    <a href='RandyR_ResumeUpdated.pdf' download='RandyR_ResumeUpdated.pdf'>
-                    <button className='btn highlighted-btn'>Get Resume</button>
-                    </a>
+            <div className="about-me-options">
+              <button
+                className="btn primary-btn"
+                onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+              >
+                {""}
+                Hire Me{" "}
+              </button>
+              <a
+                href="RandyR_ResumeUpdated.pdf"
+                download="RandyR_ResumeUpdated.pdf"
+              >
+                <button className="btn highlighted-btn">Get Resume</button>
+              </a>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- client/src/PortfolioContainer/Home/Profile/Profile.js
+++ client/src/PortfolioContainer/Home/Profile/Profile.js
@@ -1,70 +1,76 @@
-import React from 'react';
-import Typical from 'react-typical';
+import React from "react";
+import Typical from "react-typical";
 import "./Profile.css";
+import ScrollService from "../../../utilities/ScrollService";
 
 export default function Profile() {
   return (
-    <div className='profile-container'>
-        <div className='profile-parent'>
-            <div className='profile-details'>
-                <div className='colz'>
-                    <div className='colz-icon'>
-                    <a href='https://www.linkedin.com/in/randy-reina'>
-                        <i className='fa fa-linkedin-square'></i>
-                    </a>
-                    <a href='https://github.com/ReiCode28'>
-                        <i className='fa fa-github-square'></i>
-                    </a>
-                    </div>
-                </div>
-                <div className="profile-details-name">
-                    <span className="primary-text">
-                        {" "}
-                        Hello, I'm <span className="highlighted-text">Randy</span>
-                    </span>
-                </div>
-                <div className='profile-details-role'>
-                    <span className='primary-text'>
-                        {" "}
-                        <h1>
-                            {" "}
-                            <Typical 
-                            loop={Infinity}
-                            steps={[
-                                "Creative 👨‍🎨",
-                                1500,
-                                "Visionary 😎",
-                                1500,
-                                "Passionate ❤️‍🔥",
-                                1500,
-                                "Life-long learner 📚",
-                                1500,
-                                "Critical Thinker 🧠",
-                                1500,
-                            ]}
-                            />
-                        </h1>
-                        <span className='profile-role-tagline'> 
-                            Building applications with front end and back end operations.
-                        </span> 
-                    </span>
-                </div>
-                <div className='profile-options'>
-                    <button className='btn primary-btn'>
-                        {""}
-                        Hire Me{" "}
-                    </button>
-                    <a href='RandyR_ResumeUpdated.pdf' download='RandyR_ResumeUpdated.pdf'>
-                    <button className='btn highlighted-btn'>Get Resume</button>
-                    </a>
-                </div>
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="https://www.linkedin.com/in/randy-reina">
+                <i className="fa fa-linkedin-square"></i>
+              </a>
+              <a href="https://github.com/ReiCode28">
+                <i className="fa fa-github-square"></i>
+              </a>
             </div>
-            <div className='profile-picture'>
-                <div className='profile-picture-background'>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'm <span className="highlighted-text">Randy</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Creative 👨‍🎨",
+                    1500,
+                    "Visionary 😎",
+                    1500,
+                    "Passionate ❤️‍🔥",
+                    1500,
+                    "Life-long learner 📚",
+                    1500,
+                    "Critical Thinker 🧠",
+                    1500,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Building applications with front end and back end operations.
+              </span>
+            </span>
+          </div>
 
-                </div>
-            </div>
+          <div className="profile-options">
+            <button
+              className="btn primary-btn"
+              onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+            >
+              {" "}
+              Hire Me{" "}
+            </button>
+            <a
+              href="RandyR_ResumeUpdated.pdf"
+              download="RandyR_ResumeUpdated.pdf"
+            >
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
         </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
